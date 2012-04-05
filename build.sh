e2506346c91c00893afde6ebb88c8b9c2c3436ac
Laszlo Nagy <rizsotto@gmail.com>
2012-04-05 21:58:33 +0200

    pin down the basic build/test commands

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/env bash
 
 script_dir=$(readlink -m $(dirname "${BASH_SOURCE[0]}"))
+build_dir=$(pwd)
 llvm_source_dir="$script_dir/../llvm.from.svn"
 llvm_build_dir="$llvm_source_dir/build"
 
 cmake $script_dir -DLLVM_SRC_DIR=$llvm_source_dir -DLLVM_BUILD_DIR=$llvm_build_dir
+
+make VERBOSE=1
+
+$llvm_build_dir/bin/clang++ -cc1 -load $build_dir/sources/libFindConstCandidates.so -plugin print-fns $script_dir/test/first.cpp
---
+++ test/first.cpp
@@ -0,0 +1,6 @@
+int good_candidate( int k )
+{
+    int j = k + 1;
+    return j;
+}
+
