baaf4edbda308e8906992f8823200f849a134263
Laszlo Nagy <rizsotto@gmail.com>
2012-04-11 23:38:19 +0200

    pass system includes when run with plugins

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/env bash
+set -o nounset
+set -o errexit
 
 script_dir=$(readlink -m $(dirname "${BASH_SOURCE[0]}"))
 build_dir=$(pwd)
@@ -7,6 +9,9 @@ llvm_build_dir="$llvm_source_dir/build"
 
 cmake $script_dir -DLLVM_SRC_DIR=$llvm_source_dir -DLLVM_BUILD_DIR=$llvm_build_dir
 
-make VERBOSE=1
+make
 
-$llvm_build_dir/bin/clang++ -cc1 -load $build_dir/sources/libFindConstCandidates.so -plugin find-const-candidate $script_dir/test/first.cpp
+clangbin=$llvm_build_dir/bin/clang++
+stdincs=$($clangbin -v -c -x c++ /dev/null 2>&1 | grep internal | awk '{ for (i = 1; i<=NF; i++) { if (match($i, "-internal-")) { k = ++i; printf("-I%s ", $k) } } }')
+
+$llvm_build_dir/bin/clang++ -cc1 $stdincs -load $build_dir/sources/libFindConstCandidates.so -plugin find-const-candidate $script_dir/test/first.cpp
--- test/first.cpp
+++ test/first.cpp
@@ -1,6 +1,15 @@
+#include <stdio.h>
+
 int good_candidate( int k )
 {
     int j = k + 1;
     return j;
 }
 
+int another_method()
+{
+    int const j = 0;
+    int k = good_candidate( j + 1 );
+
+    return 2 * k;
+}
