0ccca021de4d8b66aac724fe8bbb41a6878cb17f
Laszlo Nagy <rizsotto@gmail.com>
2012-04-12 22:30:42 +0200

    write wrapper to pass plugin arguments

--- build.sh
+++ build.sh
@@ -11,7 +11,4 @@ cmake $script_dir -DLLVM_SRC_DIR=$llvm_source_dir -DLLVM_BUILD_DIR=$llvm_build_d
 
 make
 
-clangbin=$llvm_build_dir/bin/clang++
-stdincs=$($clangbin -v -c -x c++ /dev/null 2>&1 | grep internal | awk '{ for (i = 1; i<=NF; i++) { if (match($i, "-internal-")) { k = ++i; printf("-I%s ", $k) } } }')
-
-$llvm_build_dir/bin/clang++ -cc1 $stdincs -load $build_dir/sources/libFindConstCandidates.so -plugin find-const-candidate $script_dir/test/first.cpp
+$script_dir/wrapper $script_dir/test/first.cpp
