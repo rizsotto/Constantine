5fa61378f6b56718cf22c5b01fe4dd974fb79559
Laszlo Nagy <laszlo.3.nagy@nokia.com>
2012-04-12 12:04:14 +0000

    extend example

--- test/first.cpp
+++ test/first.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int good_candidate( int k )
+int good_candidate( int );
+
+namespace
 {
-    int j = k + 1;
-    return j;
-}
 
 int another_method()
 {
@@ -12,4 +11,32 @@ int another_method()
     int k = good_candidate( j + 1 );
 
     return 2 * k;
+}
+
+}
+
+void with_attribute() __attribute__ ((deprecated));
+
+int good_candidate( int k )
+{
+    int j = k + 1;
+    return j;
+}
+
+class AClass
+{
+    void example_method( int );
+    int const_method( int ) const;
+};
+
+void AClass::example_method( int k )
+{
+    const int * i = & k;
+    int * ii = const_cast<int *>( i );
+    int * iii = (int *)i;
+}
+
+int AClass::const_method( const int k ) const
+{
+    return good_candidate( k );
 }
