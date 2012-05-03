387ae110a9993d2fca491e538a417166cb4ef009
Laszlo Nagy <rizsotto@gmail.com>
2012-05-04 00:55:08 +0200

    implement pseudo const analysis on method arguments
    use the clang::PseudoConstantAnalysis class

--- sources/PluginMain.cpp
+++ sources/PluginMain.cpp
@@ -1,32 +1,28 @@
 // Copyright 2012 by Laszlo Nagy [see file MIT-LICENSE]
 
+#include <cassert>
 #include <clang/Basic/LangOptions.h>
 #include <clang/Basic/Diagnostic.h>
 #include <clang/Basic/SourceLocation.h>
 #include <clang/Frontend/FrontendPluginRegistry.h>
+#include <clang/Frontend/CompilerInstance.h>
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/AST/AST.h>
 #include <clang/AST/Stmt.h>
-#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Analysis/Analyses/PseudoConstantAnalysis.h>
 #include <llvm/Support/raw_ostream.h>
 
 namespace {
 
-void verboseDump(clang::Decl const * const Decl) {
-    clang::LangOptions const LO;
-    clang::PrintingPolicy PP(LO);
-    PP.Dump = true;
-
-    Decl->print(llvm::errs(), PP);
-}
-
 class VariableVisitor : public clang::ASTConsumer
                       , public clang::RecursiveASTVisitor<VariableVisitor> {
+    clang::DiagnosticsEngine & DiagEng;
 public:
-    VariableVisitor()
+    VariableVisitor(clang::CompilerInstance const & Compiler)
         : clang::ASTConsumer()
         , clang::RecursiveASTVisitor<VariableVisitor>()
+        , DiagEng(Compiler.getDiagnostics())
     { }
 
     virtual void HandleTranslationUnit(clang::ASTContext & ctx) {
@@ -35,19 +31,35 @@ public:
 
     // method arguments
     bool VisitParmVarDecl(clang::ParmVarDecl const * const Decl) {
-        return VisitFilteredVarDecl(Decl);
+        clang::DeclContext const * const Ctx = Decl->getParentFunctionOrMethod();
+        assert(Ctx);
+
+        clang::FunctionDecl const * const Function =
+            llvm::dyn_cast<clang::FunctionDecl const>(Ctx);
+        assert(Function);
+        assert(Function->hasBody());
+
+        clang::PseudoConstantAnalysis Checker(Function->getBody());
+        return (! Checker.wasReferenced(Decl))
+            ? reportNotUsed(Decl)
+            : (Checker.isPseudoConstant(Decl))
+                ? reportPseudoConst(Decl)
+                : true;
     }
 
-    // top level variable declarations
-    bool VisitVarDecl(clang::VarDecl const * const Decl) {
-        return (Decl->isFileVarDecl() && (!Decl->hasExternalStorage()))
-            ? VisitFilteredVarDecl(Decl)
-            : true;
-    }
+private:
+    bool report(clang::VarDecl const * const Decl, char const * const msg) {
+        unsigned const DiagID =
+            DiagEng.getCustomDiagID(clang::DiagnosticsEngine::Warning, msg);
+        DiagEng.Report(Decl->getLocStart(), DiagID);
 
-    bool VisitFilteredVarDecl(clang::VarDecl const * const Decl) {
-        verboseDump(Decl);
-        return true;
+        return false;
+    }
+    bool reportNotUsed(clang::VarDecl const * const Decl) {
+        return report(Decl, "variable declared, but not used [Medve plugin]");
+    }
+    bool reportPseudoConst(clang::VarDecl const * const Decl) {
+        return report(Decl, "variable could be declared as const [Medve plugin]");
     }
 };
 
@@ -67,7 +79,7 @@ class MedvePlugin : public clang::PluginASTAction {
     clang::ASTConsumer * CreateASTConsumer(clang::CompilerInstance & Compiler,
                                            llvm::StringRef) {
         return isCPlusPlus(Compiler)
-            ? (clang::ASTConsumer *) new VariableVisitor()
+            ? (clang::ASTConsumer *) new VariableVisitor(Compiler)
             : (clang::ASTConsumer *) new NullConsumer();
     }
 
