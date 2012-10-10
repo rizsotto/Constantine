de589fd83f376da1dda9811f1b9d36e9eab820e4
Laszlo Nagy <rizsotto@gmail.com>
2012-10-10 12:55:26 +0200

    adding template related test on change analysis

---
+++ test/AnalysisFindsChanges/TemplateMemthodCall.cpp
@@ -0,0 +1,33 @@
+// RUN: %clang_cc1 %change %s -fsyntax-only -verify
+
+template<typename T>
+struct TestType {
+    void change() { }
+    void not_change() const { }
+
+    template <typename M>
+    void change(M const &) { }
+
+    template <typename M>
+    void not_change(M const &) const { }
+};
+
+void test_non_const_method() {
+    TestType<int> T;
+    T.change(); // expected-note {{variable 'T' with type 'TestType<int>' was changed}}
+}
+
+void test_const_method() {
+    TestType<int> T;
+    T.not_change();
+}
+
+void test_non_const_template_method() {
+    TestType<int> T;
+    T.change('k'); // expected-note {{variable 'T' with type 'TestType<int>' was changed}}
+}
+
+void test_const_template_method() {
+    TestType<int> T;
+    T.not_change('k');
+}
