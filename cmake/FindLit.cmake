17f61d48d3a78873cb3df884d992e8e8a3456875
Laszlo Nagy <rizsotto@gmail.com>
2015-12-16 18:16:36 +0100

    travis-ci: use lit from debian package

--- cmake/FindLit.cmake
+++ cmake/FindLit.cmake
@@ -8,4 +8,8 @@ find_program(LIT_EXECUTABLE
 if(LIT_EXECUTABLE)
   set(LIT_FOUND 1)
   message(STATUS "lit found : ${LIT_EXECUTABLE}")
+elseif(LIT_MODULE)
+  set(LIT_FOUND 1)
+  set(LIT_EXECUTABLE python ${LIT_MODULE})
+  message(STATUS "lit found : ${LIT_EXECUTABLE}")
 endif()
